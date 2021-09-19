824c7805d52c24ffab2551608dcf5d7bda0e99d8
trev <Oojimentis>
2021-09-19 16:26:54 -0400

    test

--- uat978_wx_text/uat_decode.c
+++ uat978_wx_text/uat_decode.c
@@ -2387,7 +2387,7 @@ static void get_seg_graph(const struct fisb_apdu *apdu, FILE *to)
 					alt = alt_raw * 100;
 
 					if (alt != alt_save) {		// Multiple altitudes
-						fprintf(to,"**** Multiple Altitudes\n");
+						fprintf(to,"**** multiple altitudes\n");
 					}
 
 					asprintf(&coords, " [%f,%f],", lng, lat);
